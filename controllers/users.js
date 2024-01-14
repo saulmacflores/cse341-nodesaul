c5d0bf38bbe8da3bd198ef449376e73585a36c26
saulmacflores <saulrosas565@gmail.com>
2024-01-13 23:16:28 -0700

    fixed

--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,7 @@ const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
     result.toArray().then((users) => {
-        res.setHeader('content type', 'application/json');
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
 
     });
